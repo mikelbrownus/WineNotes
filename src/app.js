ace80be315a52f764e0f52cdc7a603acdf72981e
Michael L Brown <mikelbrownus@gmail.com>
2018-10-12 11:04:32 -0600

    Basic project structure added

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => <h1>Hello Wine Notes</h1>;
-
-export default App;
---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Header from './header';
+import Footer from './footer';
+import WineNotes from './wine-notes/wine-notes'
+
+const App = () => (
+    <Fragment>
+        <Header />
+        <WineNotes />
+        <Footer />
+    </Fragment>
+);
+
+export default App;
