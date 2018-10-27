7b136e72a773033e55a10bcf1c0e4bd796156cf1
Michael L Brown <mikelbrownus@gmail.com>
2018-10-27 11:02:52 -0600

    Basic responsive grid for notes

--- src/components/wine-notes/wine-notes.js
+++ src/components/wine-notes/wine-notes.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import { Grid, Paper } from '@material-ui/core';
 
 const WineNotes = () => (
-  <ul>
-    <li>One</li>
-    <li>Two</li>
-    <li>Three</li>
-  </ul>
+  <Grid
+    container
+    direction="row"
+    justify="center"
+    alignItems="center"
+  >
+    <Grid item xs={12} sm={6} md={4}>
+      <Paper>One</Paper>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <Paper>Two</Paper>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <Paper>Three</Paper>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <Paper>Four</Paper>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <Paper>Five</Paper>
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+      <Paper>Six</Paper>
+    </Grid>
+  </Grid>
 );
 
 export default WineNotes;
