4343fc13d839589f5eed6f85f28f65063d20b526
Michael L Brown <mikelbrownus@gmail.com>
2018-11-11 15:41:18 -0700

    Add CssBaseLine to make sure you have a clean slate with CSS

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Header from './header';
 import Footer from './footer';
 import WineNotes from './wine-notes/wine-notes';
 
 const App = () => (
   <Fragment>
+    <CssBaseline />
     <Header />
     <WineNotes />
     <Footer />
