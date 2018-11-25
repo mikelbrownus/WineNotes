6e0bb2d7fa06f25b45167cab6b11adfe356bcf6c
Michael L Brown <mikelbrownus@gmail.com>
2018-11-25 11:16:10 -0700

    Basic design for cards

--- src/components/wine-notes/wine-note-card.js
+++ src/components/wine-notes/wine-note-card.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import {
+  Card,
+  IconButton,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  Typography,
+  withStyles
+} from '@material-ui/core';
+import { MoreVert, LocalDrink } from '@material-ui/icons';
+
 
 const styles = {
   card: {
+    display: 'flex',
+    flexDirection: 'row',
     minWidth: 275,
     margin: '2px 0',
+    height: 125,
   },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
+  photo: {
+    alignContent: 'center',
+    padding: 4,
+    height: '100%',
   },
   pos: {
     marginBottom: 12,
+  },
+  actionArea: {
+    width: '80%',
+  },
+  actions: {
+    alignContent: 'center',
+    width: '15%',
+
+  },
+  nameLength: {
+    overflow: 'hidden',
+    lineHeight: '1em',
+    maxHeight: '2em',
+    textOverflow: 'ellipsis',
   },
 };
 
@@ -29,24 +48,23 @@ function WineNoteCard(props) {
 
   return (
     <Card className={classes.card}>
-      <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Wine Note
-        </Typography>
-        <Typography variant="title" component="h2">
-          {note.name}
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
+      <LocalDrink className={classes.photo} />
+      <CardActionArea className={classes.actionArea}>
+        <CardContent>
+          <Typography variant="subheading" component="p" className={classes.nameLength}>
+            {note.name}
+          </Typography>
+          <Typography className={classes.pos} color="textSecondary">
+          Date:
+            {' '}
+            {note.date}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions className={classes.actions}>
+        <IconButton color="inherit">
+          <MoreVert />
+        </IconButton>
       </CardActions>
     </Card>
   );
