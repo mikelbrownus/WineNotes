c6b78c2f8a4943891b190195fef767591222664e
Michael L Brown <mikelbrownus@gmail.com>
2019-01-04 13:47:57 -0700

    Waring message for help page

--- src/components/help/help.js
+++ src/components/help/help.js
@@ -3,11 +3,29 @@ import React from 'react';
 const divStyle = {
   height: 'calc(100% - 116px)',
   overflowY: 'scroll',
+  margin: '0 auto',
+  maxWidth: '650px',
 };
 
 const Help = () => (
   <div style={divStyle}>
-    <h1>This is help page</h1>
+    <h2>Warning!</h2>
+    <p>
+      This app is an experiemental app aiming towards a full fledged
+      wine tasting app using a web frontend.  There is no backend services
+      associated with this app so all data will need to be stored by the
+      client browser.  This means that if clear you data or your browser
+      decides to clean up data there is nothing that can be done about that
+      and all data will be lost.  Please use for testing purposes only.
+    </p>
+    <p>
+      This is purely
+      {' '}
+      <strong>exprimental</strong>
+!     Do not use for data you need to
+      ensure does not get lost.
+    </p>
+
   </div>
 );
 
