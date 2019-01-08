c6295213f5b59c60b796c02280e52dac64d56a39
Michael L Brown <mikelbrownus@gmail.com>
2019-01-07 18:13:33 -0700

    NoteMapper tests file added plus a single test

--- src/specs/note-mapper.test.js
+++ src/specs/note-mapper.test.js
@@ -1,11 +1,9 @@
- import NoteMapper from '../model/note-mapper';
- import initialState from '../initialState.json';
+import NoteMapper from '../model/note-mapper';
+import initialState from '../initialState.json';
 
 describe('Note mapper tests', () => {
   it('Mapper.getName should not be null', () => {
     const noteMapper = NoteMapper(initialState.WineNotes[0]);
     expect(noteMapper.getName()).not.toBeNull();
   });
-
-  
 });
