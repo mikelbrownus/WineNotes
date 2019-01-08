6405586aa868b538e0188a195f56ae48bd8f65ba
Michael L Brown <mikelbrownus@gmail.com>
2019-01-08 11:04:18 -0700

    NoteMapper constructs name + test

--- src/model/note-mapper.js
+++ src/model/note-mapper.js
@@ -1,6 +1,16 @@
-const NoteMapper = note => ({
-  getDate: () => note.date,
-  getName: () => note.name,
-});
+const NoteMapper = note => {
+  const getName = () => {
+    const m = (note.maker) ? note.maker + " " : "";
+    const n = (note.wineName) ? note.wineName + " " : "";
+    const va = (note.varietal) ? note.varietal + " " : "";
+    const r = (note.region) ? note.region + " " : "";
+    const vi = (note.vintage) ? note.vintage : "";
+    return m + n + va + r + vi
+  }
+  return {
+    getDate: () => note.date,
+    getName: getName,
+  }
+};
 
 export default NoteMapper;
--- src/specs/note-mapper.test.js
+++ src/specs/note-mapper.test.js
@@ -6,4 +6,9 @@ describe('Note mapper tests', () => {
     const noteMapper = NoteMapper(initialState.WineNotes[0]);
     expect(noteMapper.getName()).not.toBeNull();
   });
+
+  it('Mapper.getName should not be correct', () => {
+    const noteMapper = NoteMapper(initialState.WineNotes[2]);
+    expect(noteMapper.getName()).toEqual("Drouhin Roserock Chardonnay Eola-Amity Hills 2016");
+  });
 });
