703c06d9d580248da60a087564c4a2edfcecd806
Michael L Brown <mikelbrownus@gmail.com>
2019-01-07 18:12:21 -0700

    Get testing with Jest working with import statements--needed babel-env

---
+++ src/specs/note-mapper.test.js
@@ -0,0 +1,11 @@
+ import NoteMapper from '../model/note-mapper';
+ import initialState from '../initialState.json';
+
+describe('Note mapper tests', () => {
+  it('Mapper.getName should not be null', () => {
+    const noteMapper = NoteMapper(initialState.WineNotes[0]);
+    expect(noteMapper.getName()).not.toBeNull();
+  });
+
+  
+});
