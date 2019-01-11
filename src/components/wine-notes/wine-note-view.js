47f81e1ada59b0963a1c0fb7ba00ec777000eab3
Michael L Brown <mikelbrownus@gmail.com>
2019-01-10 18:33:53 -0700

    Back button for wine note view componenet

--- src/components/wine-notes/wine-note-view.js
+++ src/components/wine-notes/wine-note-view.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdKeyboardBackspace } from 'react-icons/md';
+import { Button } from '@material-ui/core';
 
 const divStyle = {
   height: 'calc(100% - 116px)',
   overflowY: 'scroll',
+  margin: '0 auto',
+  maxWidth: '650px',
 };
 
 const WineNoteView = ({ location }) => {
@@ -10,6 +15,11 @@ const WineNoteView = ({ location }) => {
   return (
     <div style={divStyle}>
       <p>{JSON.stringify(wineNote)}</p>
+      <Link to="/">
+        <Button variant="contained" color="primary">
+          <MdKeyboardBackspace />
+        </Button>
+      </Link>
     </div>
   );
 };
