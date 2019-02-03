0cdec47e5de9bd8b31d2a04c2d6fa0aba28289ce
Michael L Brown <mikelbrownus@gmail.com>
2019-02-02 20:23:28 -0700

    Image appears in wineNoteView if any

--- src/components/wine-notes/wine-note-view.js
+++ src/components/wine-notes/wine-note-view.js
@@ -1,16 +1,35 @@
 import React, { Fragment } from 'react';
 import {
-  Card, CardContent, Typography,
+  Card, CardContent, Typography, CardMedia, withStyles,
 } from '@material-ui/core';
 import NoteMapper from '../../model/note-mapper';
 import WineNoteDialog from './wine-note-dialog';
 import Context from '../../app-context';
 
-const divStyle = {
-  height: 'calc(100% - 136px)',
-  overflowY: 'scroll',
-  margin: '20px auto 0  auto',
-  maxWidth: '650px',
+const styles = {
+  body: {
+    height: 'calc(100% - 136px)',
+    overflowY: 'scroll',
+    margin: '20px auto 0  auto',
+    maxWidth: '650px',
+  },
+  media: {
+    margin: 'auto 0 auto 0',
+    width: '200px',
+    height: '100%',
+  },
+  card: {
+    display: 'flex',
+    width: '100%',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+
+  },
+  content: {
+    flex: '1 auto',
+  },
 };
 
 class WineNoteView extends React.Component {
@@ -27,41 +46,56 @@ class WineNoteView extends React.Component {
   }
 
   render() {
-    const { tastingNote, technicalNote } = this.state;
+    const { tastingNote, technicalNote, image } = this.state;
+    const { classes } = this.props;
     return (
-      <div style={divStyle}>
+      <div className={classes.body}>
         <Context.Consumer>
           { context => (
             <Fragment>
-              <Card>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {this.getMapper().getName()}
-                  </Typography>
-                  {tastingNote && (
-                  <Fragment>
-                    <Typography variant="h6" component="h3">
+              <Card className={classes.card}>
+                { image
+                    && (
+                      <CardMedia
+                        className={classes.media}
+                        image={image}
+                        title={this.getMapper().getName()}
+                        component="img"
+                      />
+
+                    )
+                }
+                <div className={classes.details}>
+                  <CardContent className={classes.content}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {this.getMapper().getName()}
+                    </Typography>
+                    {tastingNote && (
+                    <Fragment>
+                      <Typography variant="h6" component="h3">
               Tasting Notes
-                    </Typography>
-                    <Typography component="p">
-                      {tastingNote}
-                    </Typography>
-                  </Fragment>
-                  )
+                      </Typography>
+                      <Typography component="p">
+                        {tastingNote}
+                      </Typography>
+                    </Fragment>
+                    )
             }
-                  {technicalNote && (
-                  <Fragment>
-                    <Typography variant="h6" component="h3">
+                    {technicalNote && (
+                    <Fragment>
+                      <Typography variant="h6" component="h3">
               Technical Notes
-                    </Typography>
-                    <Typography component="p">
-                      {technicalNote}
-                    </Typography>
-                  </Fragment>
-                  )
+                      </Typography>
+                      <Typography component="p">
+                        {technicalNote}
+                      </Typography>
+                    </Fragment>
+                    )
             }
-                </CardContent>
+                  </CardContent>
+                </div>
               </Card>
+
               <WineNoteDialog
                 handleClose={() => { context.state.setNoteDialog(false); }}
                 open={context.state.editDialogOpen}
@@ -77,4 +111,4 @@ class WineNoteView extends React.Component {
 }
 
 
-export default WineNoteView;
+export default withStyles(styles)(WineNoteView);
