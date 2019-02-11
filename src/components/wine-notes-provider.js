a4c6760c39f2967542b07fe4c001af853b99959d
Michael L Brown <mikelbrownus@gmail.com>
2019-02-11 13:15:12 -0700

    Define functions outside of constructor

--- src/components/wine-notes-provider.js
+++ src/components/wine-notes-provider.js
@@ -9,57 +9,6 @@ const repository = WineNoteRepository();
 class WineNotesProvider extends React.Component {
   constructor(props) {
     super(props);
-    this.addTestData = () => {
-      if (repository.getNotes().length === 0) {
-        repository.setNotes(initialState.WineNotes);
-      } else {
-        repository.deleteAll();
-        repository.setNotes(initialState.WineNotes);
-      }
-
-      this.setState(() => ({
-        WineNotes: repository.filteredNotes(),
-      }));
-    };
-
-    this.deleteNote = (id) => {
-      repository.deleteNote(id);
-      this.setState(() => ({
-        WineNotes: repository.filteredNotes(),
-      }));
-    };
-
-    this.filterNotes = (filter) => {
-      this.setState(() => ({
-        WineNotes: repository.filteredNotes(filter),
-      }));
-    };
-
-    this.editNoteDialogToggle = () => {
-      this.setState(prevState => ({
-        editDialogOpen: !prevState.editDialogOpen,
-      }));
-    };
-
-    this.setNoteDialog = (open) => {
-      this.setState(() => ({
-        editDialogOpen: open,
-      }));
-    };
-
-    this.updateNote = (id, changes) => {
-      repository.update(id, changes);
-      this.setState(() => ({
-        WineNotes: repository.filteredNotes(),
-      }));
-    };
-
-    this.addNote = (note) => {
-      repository.insert(note);
-      this.setState(() => ({
-        WineNotes: repository.filteredNotes(),
-      }));
-    };
 
     this.state = {
       WineNotes: repository.filteredNotes(),
@@ -74,6 +23,57 @@ class WineNotesProvider extends React.Component {
     };
   }
 
+  addTestData = () => {
+    if (repository.getNotes().length === 0) {
+      repository.setNotes(initialState.WineNotes);
+    } else {
+      repository.deleteAll();
+      repository.setNotes(initialState.WineNotes);
+    }
+
+    this.setState(() => ({
+      WineNotes: repository.filteredNotes(),
+    }));
+  };
+
+  deleteNote = (id) => {
+    repository.deleteNote(id);
+    this.setState(() => ({
+      WineNotes: repository.filteredNotes(),
+    }));
+  };
+
+  filterNotes = (filter) => {
+    this.setState(() => ({
+      WineNotes: repository.filteredNotes(filter),
+    }));
+  };
+
+  editNoteDialogToggle = () => {
+    this.setState(prevState => ({
+      editDialogOpen: !prevState.editDialogOpen,
+    }));
+  };
+
+  setNoteDialog = (open) => {
+    this.setState(() => ({
+      editDialogOpen: open,
+    }));
+  };
+
+  updateNote = (id, changes) => {
+    repository.update(id, changes);
+    this.setState(() => ({
+      WineNotes: repository.filteredNotes(),
+    }));
+  };
+
+  addNote = (note) => {
+    repository.insert(note);
+    this.setState(() => ({
+      WineNotes: repository.filteredNotes(),
+    }));
+  };
 
   render() {
     const { children } = this.props;
