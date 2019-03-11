fe21f00453792a9fccdcde9d0b3a5ffebe022f68
Michael L Brown <mikelbrownus@gmail.com>
2019-03-11 14:49:52 -0600

    skipWaiting false is safer

--- webpack.common.js
+++ webpack.common.js
@@ -29,7 +29,7 @@ module.exports = {
       ]),
       new WorkboxPlugin.GenerateSW({
           clientsClaim: true, 
-          skipWaiting: true,
+          skipWaiting: false,
           runtimeCaching: [
             {
                 urlPattern: /icons/,
