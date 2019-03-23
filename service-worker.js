2b1090b14e7f6f5972306aed019cb616279d209b
Michael L Brown <mikelbrownus@gmail.com>
2019-03-23 10:15:21 -0600

    Updates

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.1.0/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.816f0cea7045a9aef6346afceb2781f3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerRoute(/icons/, new workbox.strategies.CacheFirst(), 'GET');
+workbox.routing.registerRoute(/^https:\/\/fonts.(?:googleapis|gstatic).com\/(.*)/, new workbox.strategies.StaleWhileRevalidate({ "cacheName":"google-fonts-stylesheets", plugins: [] }), 'GET');
+workbox.routing.registerRoute(/.*/, new workbox.strategies.NetworkFirst(), 'GET');
+workbox.routing.registerRoute(/(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg|gif|png|svg))/, new workbox.strategies.NetworkFirst({ "cacheName":"external-images", plugins: [new workbox.expiration.Plugin({ maxEntries: 200, maxAgeSeconds: 1209600, purgeOnQuotaError: false })] }), 'GET');
