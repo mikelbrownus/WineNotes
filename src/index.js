bb9ea2a936caf344358dda200d31fbb085ad84ab
Michael L Brown <mikelbrownus@gmail.com>
2019-03-23 10:50:55 -0600

    Relative path for service worker

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV && process.env.NODE_ENV === 'production') {
   // production code
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js');
+      navigator.serviceWorker.register('./service-worker.js');
     });
   }
 }
