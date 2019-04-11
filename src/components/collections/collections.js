ae25ca88e8e6e455b5ffd42852038edbcd6b06c3
Mike Brown <mlb@russell.local>
2019-04-11 10:24:40 -0600

    Setup grid for collections

--- src/components/collections/collections.js
+++ src/components/collections/collections.js
@@ -1,14 +1,52 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Fab from '@material-ui/core/Fab';
+import { MdAdd } from 'react-icons/md';
+import withStyles from '@material-ui/core/styles/withStyles';
 
-const divStyle = {
-  height: 'calc(100% - 116px)',
-  overflowY: 'scroll',
+const styles = theme => ({
+  gridStyle: {
+    height: 'calc(100% - 132px)',
+    margin: '8px 4px',
+    [theme.breakpoints.up('sm')]: {
+      margin: '8px 16px',
+    },
+    overflowY: 'auto',
+  },
+  fab: {
+    position: 'absolute',
+    bottom: '64px',
+    right: '20px',
+  },
+});
+
+const Collections = (props) => {
+  const { classes } = props;
+  return (
+    <div className={classes.gridStyle}>
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item xs={12} sm={6} md={4}>
+          <h3>Item 1</h3>
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <h3>Item 2</h3>
+        </Grid>
+      </Grid>
+      <Fab
+        aria-label="Add"
+        color="primary"
+        size="small"
+        className={classes.fab}
+      >
+        <MdAdd />
+      </Fab>
+    </div>
+  );
 };
 
-const Collections = () => (
-  <div style={divStyle}>
-    <h1>This is collections page</h1>
-  </div>
-);
-
-export default Collections;
+export default withStyles(styles)(Collections);
