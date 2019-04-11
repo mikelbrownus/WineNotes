97425f9f3e291cb178ed1a4f26b411d296949cba
Mike Brown <mlb@russell.local>
2019-04-10 18:57:57 -0600

    Better variable names for mapper

--- src/model/note-mapper.js
+++ src/model/note-mapper.js
@@ -1,21 +1,21 @@
 const NoteMapper = note => {
   const getName = (order) => {
-    const m = note.maker ? `${note.maker} ` : '';
-    const n = note.wineName ? `${note.wineName} ` : '';
-    const va = note.varietal && !note.varietal.startsWith('Blend')
+    const maker = note.maker ? `${note.maker} ` : '';
+    const name = note.wineName ? `${note.wineName} ` : '';
+    const varietal = note.varietal && !note.varietal.startsWith('Blend')
       ? `${note.varietal} `
       : '';
-    const r = note.region ? `${note.region} ` : '';
-    const vi = note.vintage && note.nonvintage === false ? `${note.vintage} ` : '';
+    const region = note.region ? `${note.region} ` : '';
+    const vintage = note.vintage && note.nonvintage === false ? `${note.vintage} ` : '';
     switch (order) {
       case 1:
-        return vi + m + n + va + r;
+        return vintage + maker + name + varietal + region;
       case 2:
-        return n + m + va + r + vi;
+        return name + maker + varietal + region + vintage;
       case 3:
-        return m + r + n + va + vi;
+        return maker + region + name + varietal + vintage;
       default:
-        return m + n + va + r + vi;
+        return maker + name + varietal + region + vintage;
     }
   };
   return {
