58f37a1dce4517cd989e78c636d14f16a2f65e4f
Michael L Brown <mikelbrownus@gmail.com>
2019-06-10 11:00:03 -0600

    Convert to function with useState

--- src/components/collections/collections.js
+++ src/components/collections/collections.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import { MdAdd } from 'react-icons/md';
@@ -29,66 +29,62 @@ const styles = theme => ({
   },
 });
 
-class Collections extends React.Component {
-  state = {
-    open: false,
+const Collections = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
   };
 
-  handleOpen = () => {
-    this.setState({ open: true });
+  const handleClose = () => {
+    setOpen(false);
   };
 
-  handleClose = () => {
-    this.setState({ open: false });
-  };
+  const { classes } = props;
 
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-    return (
-      <Context.Consumer>
-        {context => (
-          <div className={classes.gridStyle}>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-            >
-              {context.state.Collections.length > 0
-                && context.state.Collections.map(collection => (
-                  <Grid item xs={12} sm={6} md={4} key={collection.id}>
-                    <CollectionCard collection={collection} />
-                  </Grid>
-                ))}
-              {context.state.Collections.length < 1 && (
-                <Typography
-                  variant="h5"
-                  component="p"
-                  className={classes.noNoteMessage}
-                >
-                  Press + to add a collection
-                </Typography>
-              )}
-            </Grid>
-            <Fab
-              aria-label="Add"
-              color="primary"
-              size="small"
-              className={classes.fab}
-              onClick={this.handleOpen}
-            >
-              <MdAdd />
-            </Fab>
-            <CollectionDialog
-              handleClose={this.handleClose}
-              open={open}
-            />
-          </div>
-        )}
-      </Context.Consumer>
-    );
-  }
-}
+  return (
+    <Context.Consumer>
+      {context => (
+        <div className={classes.gridStyle}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            {context.state.Collections.length > 0
+              && context.state.Collections.map(collection => (
+                <Grid item xs={12} sm={6} md={4} key={collection.id}>
+                  <CollectionCard collection={collection} />
+                </Grid>
+              ))}
+            {context.state.Collections.length < 1 && (
+              <Typography
+                variant="h5"
+                component="p"
+                className={classes.noNoteMessage}
+              >
+                Press + to add a collection
+              </Typography>
+            )}
+          </Grid>
+          <Fab
+            aria-label="Add"
+            color="primary"
+            size="small"
+            className={classes.fab}
+            onClick={handleOpen}
+          >
+            <MdAdd />
+          </Fab>
+          <CollectionDialog
+            handleClose={handleClose}
+            open={open}
+          />
+        </div>
+      )}
+    </Context.Consumer>
+  );
+};
 
 export default withStyles(styles)(Collections);
