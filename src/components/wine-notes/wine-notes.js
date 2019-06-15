8c13e0a7276917e2c8e11d7aaaabec3d86f2b11d
mlb <mlb@mlbs-MacBook-Pro.local>
2019-06-15 11:18:34 -0600

    Updated for useState

--- src/components/wine-notes/wine-notes.js
+++ src/components/wine-notes/wine-notes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Fab from '@material-ui/core/Fab';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -27,74 +27,72 @@ const styles = theme => ({
     display: 'block',
   },
 });
-class WineNotes extends React.Component {
-  state = {
-    open: false,
+
+const WineNotes = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
   };
 
-  handleOpen = () => {
-    this.setState({ open: true });
+  const handleClose = () => {
+    setOpen(false);
   };
 
-  handleClose = () => {
-    this.setState({ open: false });
-  };
+  const { classes } = props;
 
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-    return (
-      <Context.Consumer>
-        {context => (
-          <div className={classes.gridSize}>
-            <Grid
-              container
-              direction="row"
-              justify="center"
-              alignItems="center"
-            >
-              {context.state.WineNotes.filter(
+  return (
+    <Context.Consumer>
+      {context => (
+        <div className={classes.gridSize}>
+          <Grid
+            container
+            direction="row"
+            justify="center"
+            alignItems="center"
+          >
+            {context.state.WineNotes.filter(
+              (item) => !item.collection,
+            ).length > 0
+              && context.state.WineNotes.filter(
                 (item) => !item.collection,
-              ).length > 0
-                && context.state.WineNotes.filter(
-                  (item) => !item.collection,
-                ).map(note => (
-                  <Grid item xs={12} sm={6} md={4} key={note.id}>
-                    <WineNoteCard note={note} order={context.state.settings.nameOrder} />
-                  </Grid>
-                ))}
-              {context.state.WineNotes.filter(
-                (item) => !item.collection,
-              ).length < 1
-                   && (
-                   <Typography
-                     variant="h5"
-                     component="p"
-                     className={classes.noNoteMessage}
-                   >
+              ).map(note => (
+                <Grid item xs={12} sm={6} md={4} key={note.id}>
+                  <WineNoteCard note={note} order={context.state.settings.nameOrder} />
+                </Grid>
+              ))}
+            {context.state.WineNotes.filter(
+              (item) => !item.collection,
+            ).length < 1
+              && (
+                <Typography
+                  variant="h5"
+                  component="p"
+                  className={classes.noNoteMessage}
+                >
                   Press + to add a note
-                   </Typography>
-                   )}
-            </Grid>
-            <Fab
-              aria-label="Add"
-              color="primary"
-              size="small"
-              onClick={this.handleOpen}
-              className={classes.fab}
-            >
-              <MdAdd />
-            </Fab>
-            <WineNoteDialog
-              handleClose={this.handleClose}
-              open={open}
-              settings={context.state.settings}
-            />
-          </div>
-        )}
-      </Context.Consumer>
-    );
-  }
-}
+                </Typography>
+              )}
+          </Grid>
+          <Fab
+            aria-label="Add"
+            color="primary"
+            size="small"
+            onClick={handleOpen}
+            className={classes.fab}
+          >
+            <MdAdd />
+          </Fab>
+          <WineNoteDialog
+            handleClose={handleClose}
+            open={open}
+            settings={context.state.settings}
+          />
+        </div>
+      )}
+    </Context.Consumer>
+  );
+};
+
 
 export default withStyles(styles)(WineNotes);
