c473253b4de91ead7e5779d23f6b24d17abac9c0
Mike L Brown <mikelbrownus@gmail.com>
2019-06-16 13:53:13 -0600

    Update wine note view to useState

--- src/components/wine-notes/wine-note-view.js
+++ src/components/wine-notes/wine-note-view.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
@@ -34,94 +34,92 @@ const styles = {
   },
 };
 
-class WineNoteView extends React.Component {
-  constructor(props) {
-    super(props);
-    const { location } = props;
-    this.state = location && location.state ? location.state.wineNote : {};
-    this.order = location && location.state ? location.state.order : 0;
-  }
+const WineNoteView = (props) => {
+  const { classes, location } = props;
+  const wn = location && location.state ? location.state.wineNote : {};
+  const order = location && location.state ? location.state.order : 0;
+  const [wineNote, setwineNote] = useState(wn);
 
-  getMapper = () => NoteMapper(this.state);
+  const getMapper = () => NoteMapper(wineNote);
 
-  changeNote = note => {
-    this.setState(note);
+  const changeNote = note => {
+    setwineNote(note);
   };
 
-  render() {
-    const {
-      tastingNote, technicalNote, image, rating,
-    } = this.state;
-    const { classes } = this.props;
-    return (
-      <div className={classes.body}>
-        <Context.Consumer>
-          {context => (
-            <Fragment>
-              <Card className={classes.card}>
-                {image && (
-                  <CardMedia
-                    className={classes.media}
-                    image={image}
-                    title={this.getMapper().getName(this.order)}
-                    component="img"
-                  />
-                )}
-                <div className={classes.details}>
-                  <CardContent className={classes.content}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      {this.getMapper().getName(this.order)}
-                    </Typography>
-                    {tastingNote && (
-                      <Fragment>
-                        <Typography variant="h6" component="h3">
-                          Tasting Notes
-                        </Typography>
-                        <Typography component="div">
-                          {tastingNote.split('\n').map(i => (
-                            <div key={uuidv1()}>{i}</div>
-                          ))}
-                        </Typography>
-                      </Fragment>
-                    )}
-                    {technicalNote && (
-                      <Fragment>
-                        <Typography variant="h6" component="h3">
-                          Technical Notes
-                        </Typography>
-                        <Typography component="div">
-                          {technicalNote.split('\n').map(i => (
-                            <div key={uuidv1()}>{i}</div>
-                          ))}
-                        </Typography>
-                      </Fragment>
-                    )}
 
-                    {rating ? (
+  const {
+    tastingNote, technicalNote, image, rating,
+  } = wineNote;
+
+  return (
+    <div className={classes.body}>
+      <Context.Consumer>
+        {context => (
+          <Fragment>
+            <Card className={classes.card}>
+              {image && (
+                <CardMedia
+                  className={classes.media}
+                  image={image}
+                  title={getMapper().getName(order)}
+                  component="img"
+                />
+              )}
+              <div className={classes.details}>
+                <CardContent className={classes.content}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {getMapper().getName(order)}
+                  </Typography>
+                  {tastingNote && (
+                    <Fragment>
                       <Typography variant="h6" component="h3">
-                      Rating:
-                        {' '}
-                        {rating}
+                        Tasting Notes
                       </Typography>
-                    ) : ''}
-                  </CardContent>
-                </div>
-              </Card>
+                      <Typography component="div">
+                        {tastingNote.split('\n').map(i => (
+                          <div key={uuidv1()}>{i}</div>
+                        ))}
+                      </Typography>
+                    </Fragment>
+                  )}
+                  {technicalNote && (
+                    <Fragment>
+                      <Typography variant="h6" component="h3">
+                        Technical Notes
+                      </Typography>
+                      <Typography component="div">
+                        {technicalNote.split('\n').map(i => (
+                          <div key={uuidv1()}>{i}</div>
+                        ))}
+                      </Typography>
+                    </Fragment>
+                  )}
 
-              <WineNoteDialog
-                handleClose={() => {
-                  context.state.setNoteDialog(false);
-                }}
-                open={context.state.editDialogOpen}
-                wineNote={this.state}
-                updateNote={this.changeNote}
-              />
-            </Fragment>
-          )}
-        </Context.Consumer>
-      </div>
-    );
-  }
-}
+                  {rating ? (
+                    <Typography variant="h6" component="h3">
+                      Rating:
+                      {' '}
+                      {rating}
+                    </Typography>
+                  ) : ''}
+                </CardContent>
+              </div>
+            </Card>
+
+            <WineNoteDialog
+              handleClose={() => {
+                context.state.setNoteDialog(false);
+              }}
+              open={context.state.editDialogOpen}
+              wineNote={wineNote}
+              updateNote={changeNote}
+            />
+          </Fragment>
+        )}
+      </Context.Consumer>
+    </div>
+  );
+};
+
 
 export default withStyles(styles)(WineNoteView);
