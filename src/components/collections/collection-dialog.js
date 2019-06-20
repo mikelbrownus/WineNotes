4a9dbf9bec038d7237130222a777663c1ce14bba
Mike L Brown <mikelbrownus@gmail.com>
2019-06-19 18:24:14 -0600

    Added useState

--- src/components/collections/collection-dialog.js
+++ src/components/collections/collection-dialog.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -8,131 +8,110 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Context from '../../app-context';
 
 
-class CollectionDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    const { collection } = this.props;
-    if (collection) {
-      const {
-        name,
-        description,
-        id,
-      } = collection;
-      this.state = {
-        name,
-        description,
-        id,
-      };
-    } else {
-      this.state = {
-        name: '',
-        description: '',
-        id: '',
-      };
-    }
-  }
+const CollectionDialog = (props) => {
+  const { collection, open, handleClose } = props;
+  const theCollection = collection || { name: '', id: '', description: '' };
+  const [coll, setCollection] = useState(theCollection);
 
-  clearForm = () => {
-    this.setState({
+  const clearForm = () => {
+    setCollection({
       name: '',
       description: '',
+      id: '',
     });
   };
 
-  handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const handleChange = event => {
+    setCollection({ ...coll, [event.target.name]: event.target.value });
   };
 
-  render() {
-    const {
-      open, handleClose,
-    } = this.props;
-    const {
-      name,
-      description,
-      id,
-    } = this.state;
-    return (
-      <Context.Consumer>
-        {context => (
-          <Dialog
-            disableBackdropClick
-            disableEscapeKeyDown
-            open={open}
-            onClose={handleClose}
-            aria-labelledby="form-dialog-title"
-          >
-            <DialogTitle id="form-dialog-title">Collection</DialogTitle>
-            <DialogContent>
-              <TextField
-                autoFocus
-                margin="dense"
-                id="name"
-                name="name"
-                value={name}
-                label="Collection Name"
-                type="text"
-                onChange={this.handleChange}
-                fullWidth
-                required
-              />
-              <TextField
-                id="description"
-                name="description"
-                value={description}
-                label="Description"
-                multiline
-                rowsMax="4"
-                margin="normal"
-                variant="outlined"
-                onChange={this.handleChange}
-                fullWidth
-              />
-            </DialogContent>
-            <DialogActions>
-              <Button
-                onClick={() => {
-                  if (!id) {
-                    this.clearForm();
-                  }
-                  handleClose();
-                }}
-                color="primary"
-              >
-                Cancel
-              </Button>
-              <Button
-                onClick={
-                  id
-                    ? () => {
-                      if (!name) {
-                        context.state.updateCollection({ name: 'collection', description });
-                      } else {
-                        context.state.updateCollection(this.state);
-                      }
-                      this.clearForm();
-                      handleClose();
-                    }
-                    : () => {
-                      if (!name) {
-                        context.state.addCollection({ name: 'collection', description });
-                      } else {
-                        context.state.addCollection(this.state);
-                      }
-                      this.clearForm();
-                      handleClose();
-                    }
+  useEffect(() => {
+    if (collection) {
+      setCollection(collection);
+    }
+  }, [collection]);
+
+  return (
+    <Context.Consumer>
+      {context => (
+        <Dialog
+          disableBackdropClick
+          disableEscapeKeyDown
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Collection</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              name="name"
+              value={coll.name}
+              label="Collection Name"
+              type="text"
+              onChange={handleChange}
+              fullWidth
+              required
+            />
+            <TextField
+              id="description"
+              name="description"
+              value={coll.description}
+              label="Description"
+              multiline
+              rowsMax="4"
+              margin="normal"
+              variant="outlined"
+              onChange={handleChange}
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={() => {
+                if (!coll.id) {
+                  clearForm();
                 }
-                color="primary"
-              >
-                {id ? 'Update' : 'Add'}
-              </Button>
-            </DialogActions>
-          </Dialog>
-        )}
-      </Context.Consumer>
-    );
-  }
-}
+                handleClose();
+              }}
+              color="primary"
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={
+                coll.id
+                  ? () => {
+                    if (!coll.name) {
+                      context.state.updateCollection({ name: 'Collection', description: coll.description, id: coll.id });
+                    } else {
+                      context.state.updateCollection(coll);
+                    }
+                    clearForm();
+                    handleClose();
+                  }
+                  : () => {
+                    if (!coll.name) {
+                      context.state.addCollection({ name: 'Collection', description: coll.description });
+                    } else {
+                      context.state.addCollection(coll);
+                    }
+                    clearForm();
+                    handleClose();
+                  }
+              }
+              color="primary"
+            >
+              {coll.id ? 'Update' : 'Add'}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </Context.Consumer>
+  );
+};
+
 
 export default CollectionDialog;
