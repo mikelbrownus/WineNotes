6e13c13cf44d47a7570c735017ff2be943b77458
Mike L Brown <mikelbrownus@gmail.com>
2019-08-27 18:49:31 -0600

    Slider now in core

--- src/components/wine-notes/wine-note-dialog.js
+++ src/components/wine-notes/wine-note-dialog.js
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import Slider from '@material-ui/lab/Slider';
+import Slider from '@material-ui/core/Slider';
 import Typography from '@material-ui/core/Typography';
 import Select from '@material-ui/core/Select';
 import DialogTitle from '@material-ui/core/DialogTitle';
